4d422698c1bee5b89a57046f2d76f2f4723abf65
Emiliyan Gospodinov <emonidi@gmail.com>
2019-07-07 09:50:26 +0200

    env.port

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'src'));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
